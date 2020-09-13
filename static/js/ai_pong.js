c4e484da37b9581718f80b8b46d896f0edf71b3a
Spencer Bertsch <spencerbertsch99@gmail.com>
2020-09-13 16:41:46 -0400

    update pong ai project

--- static/js/ai_pong.js
+++ static/js/ai_pong.js
@@ -1,3 +1,7 @@
+// The source for this project can be found here: https://thoughtbot.com/blog/pong-clone-in-javascript
+// much of the JS code was adapted from this souece. this was my starting place for the pong game - I 
+// then went in and added a few other AI opponents which users could play against
+
 // Use requestAnimationFrame to animate the game
 // similar to setTimeout, but more optimized for the browser
 var animate = window.requestAnimationFrame ||
@@ -109,3 +113,157 @@ var render = function() {
     computer.render();
     ball.render();
   };
+
+// add animation to the ball
+var update = function() {
+    ball.update();
+};
+
+// update the ball's position based on the speed
+Ball.prototype.update = function() {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+};
+
+// add in the animation for the paddle
+var update = function() {
+    ball.update(player.paddle, computer.paddle);
+  };
+  
+// add bounderies for the ball
+Ball.prototype.update = function(paddle1, paddle2) {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    var top_x = this.x - 5;
+    var top_y = this.y - 5;
+    var bottom_x = this.x + 5;
+    var bottom_y = this.y + 5;
+  
+    if(this.x - 5 < 0) { // hitting the left wall
+      this.x = 5;
+      this.x_speed = -this.x_speed;
+    } else if(this.x + 5 > 400) { // hitting the right wall
+      this.x = 395;
+      this.x_speed = -this.x_speed;
+    }
+  
+    if(this.y < 0 || this.y > 600) { // a point was scored
+      this.x_speed = 0;
+      this.y_speed = 3;
+      this.x = 200;
+      this.y = 300;
+    }
+  
+    if(top_y > 300) {
+      if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+        // hit the player's paddle
+        this.y_speed = -3;
+        this.x_speed += (paddle1.x_speed / 2);
+        this.y += this.y_speed;
+      }
+    } else {
+      if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+        // hit the computer's paddle
+        this.y_speed = 3;
+        this.x_speed += (paddle2.x_speed / 2);
+        this.y += this.y_speed;
+      }
+    }
+  };
+
+// add controls
+var keysDown = {};
+
+window.addEventListener("keydown", function(event) {
+  keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+  delete keysDown[event.keyCode];
+});
+
+var update = function() {
+    player.update();
+    ball.update(player.paddle, computer.paddle);
+  };
+  
+  Player.prototype.update = function() {
+    for(var key in keysDown) {
+      var value = Number(key);
+      if(value == 37) { // left arrow
+        this.paddle.move(-4, 0);
+      } else if (value == 39) { // right arrow
+        this.paddle.move(4, 0);
+      } else {
+        this.paddle.move(0, 0);
+      }
+    }
+  };
+  
+  Paddle.prototype.move = function(x, y) {
+    this.x += x;
+    this.y += y;
+    this.x_speed = x;
+    this.y_speed = y;
+    if(this.x < 0) { // all the way to the left
+      this.x = 0;
+      this.x_speed = 0;
+    } else if (this.x + this.width > 400) { // all the way to the right
+      this.x = 400 - this.width;
+      this.x_speed = 0;
+    }
+  }
+
+// DEFINE RULES BASED AI
+
+var update = function() {
+    player.update();
+    computer.update(ball);
+    ball.update(player.paddle, computer.paddle);
+  };
+  
+  Computer.prototype.update = function(ball) {
+    var x_pos = ball.x;
+    var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
+    if(diff < 0 && diff < -4) { // max speed left
+      diff = -5;
+    } else if(diff > 0 && diff > 4) { // max speed right
+      diff = 5;
+    }
+    this.paddle.move(diff, 0);
+    if(this.paddle.x < 0) {
+      this.paddle.x = 0;
+    } else if (this.paddle.x + this.paddle.width > 400) {
+      this.paddle.x = 400 - this.paddle.width;
+    }
+  };
+
+// --- Choose Rules Based AI --- 
+document.getElementById("rules_AI").onclick = function(){
+    // todo 
+
+    // Flash success message after network parameters get updated 
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+// --- Tensorflow AI --- 
+document.getElementById("tf_AI").onclick = function(){
+    // todo 
+
+    // Flash success message after network parameters get updated 
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+// --- Reinforcement Learning AI --- 
+document.getElementById("rf_AI").onclick = function(){
+    // todo 
+
+    // Flash success message after network parameters get updated 
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
