044b1e391d9b4b84fa090593b23abfee2f1f4612
Spencer Bertsch <spencerbertsch99@gmail.com>
2020-10-02 22:09:03 -0400

    start game on space bar

--- static/js/ai_pong.js
+++ static/js/ai_pong.js
@@ -134,51 +134,14 @@ Ball.prototype.update = function() {
 var update = function() {
     ball.update(player.paddle, computer.paddle);
   };
-  
-// add bounderies for the ball
-Ball.prototype.update = function(paddle1, paddle2) {
-    this.x += this.x_speed;
-    this.y += this.y_speed;
-    var top_x = this.x - 5;
-    var top_y = this.y - 5;
-    var bottom_x = this.x + 5;
-    var bottom_y = this.y + 5;
-  
-    if(this.x - 5 < 0) { // hitting the left wall
-      this.x = 5;
-      this.x_speed = -this.x_speed;
-    } else if(this.x + 5 > 400) { // hitting the right wall
-      this.x = 395;
-      this.x_speed = -this.x_speed;
-    }
-  
-    if(this.y < 0 || this.y > 600) { // a point was scored
-      this.x_speed = 0;
-      this.y_speed = 3;
-      this.x = 200;
-      this.y = 300;
-    }
-  
-    if(top_y > 300) {
-      if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
-        // hit the player's paddle
-        this.y_speed = -3;
-        this.x_speed += (paddle1.x_speed / 2);
-        this.y += this.y_speed;
-      }
-    } else {
-      if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
-        // hit the computer's paddle
-        this.y_speed = 3;
-        this.x_speed += (paddle2.x_speed / 2);
-        this.y += this.y_speed;
-      }
-    }
-  };
+
 
 // add controls
+// we now need to add the controls before we define the Ball.prototype.update
 var keysDown = {};
+var spaceDown = {}; // add another array to be filled by space down 
 
+// for paddle controls 
 window.addEventListener("keydown", function(event) {
   keysDown[event.keyCode] = true;
 });
@@ -187,8 +150,20 @@ window.addEventListener("keyup", function(event) {
   delete keysDown[event.keyCode];
 });
 
+// for next game start 
+window.addEventListener("keydown", function(event) {
+  spaceDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+  delete spaceDown[event.keyCode];
+});
+
 var update = function() {
+    // call update method for player object
     player.update();
+
+    // call update method for ball object 
     ball.update(player.paddle, computer.paddle);
   };
   
@@ -219,8 +194,71 @@ var update = function() {
     }
   }
 
-// DEFINE RULES BASED AI
+  // add bounderies for the ball
+Ball.prototype.update = function(paddle1, paddle2) {
+  this.x += this.x_speed;
+  this.y += this.y_speed;
+  var top_x = this.x - 5;
+  var top_y = this.y - 5;
+  var bottom_x = this.x + 5;
+  var bottom_y = this.y + 5;
 
+  if(this.x - 5 < 0) { // hitting the left wall
+    this.x = 5;
+    this.x_speed = -this.x_speed;
+  } else if(this.x + 5 > 400) { // hitting the right wall
+    this.x = 395;
+    this.x_speed = -this.x_speed;
+  }
+
+  if(top_y > 300) {
+    if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+      // hit the player's paddle
+      this.y_speed = -3;
+      this.x_speed += (paddle1.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  } else {
+    if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+      // hit the computer's paddle
+      this.y_speed = 3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  }
+
+  // YOU LOSE A POINT 
+  if (this.y < 0) { 
+    this.x_speed = 0;
+    this.y_speed = 0; // <-- change the y speed to 0 until button is pressed! 
+    this.x = 200;
+    this.y = 300;
+    }
+
+  // YOU WIN A POINT 
+  if (this.y > 600) { 
+    this.x_speed = 0;
+    this.y_speed = 0; // <-- change the y speed to 0 until button is pressed! 
+    this.x = 200;
+    this.y = 300;
+    }
+
+    // after winning a point or losing a point, press the space bar to begin the next game 
+    for(var key in spaceDown) {
+      var value = Number(key);
+      // 
+      if(value == 32) { // space bar
+        this.x_speed = 0;
+        this.y_speed = 3; // <-- change the y speed to 0 until button is pressed! 
+        this.x = 200;
+        this.y = 300;
+      } 
+    }
+
+};
+
+
+// DEFINE RULES BASED AI
 var update = function() {
     player.update();
     computer.update(ball);
